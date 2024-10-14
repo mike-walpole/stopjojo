d6284ec65d79b99a505a7bbaba31e89352fd1879
mike-walpole <walpole.mike@gmail.com>
2024-10-14 13:18:05 +0200

    new footer ref

--- src/routes/kurs/+page.svelte
+++ src/routes/kurs/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import  Footer  from '$lib/components/Footer.svelte';
+  import  Footer  from '$lib/components/footer.svelte';
   import { fade } from 'svelte/transition';
 
   let activeTab = 'opis';
